0f0087393ea4f646e7b49ba605c99a7563f07b98
rjp301 <rileypaul96@gmail.com>
2022-01-19 20:46:12 -0700

    Implemented command relay, list command, help command, and download placeholder

--- client.js
+++ client.js
@@ -1,10 +1,31 @@
 const net = require('net');
+const readline = require('readline');
 const { PORT, HOST } = require('./constants');
 
+console.log("Connecting...");
 const conn = net.createConnection({
-  host: 'localhost',
-  port: 8080
+  host: HOST,
+  port: PORT
+});
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
 });
 
 conn.setEncoding("utf8");
 
+conn.on('connect', () => {
+  console.log("Connection successful");
+});
+
+// Log all data from server
+conn.on('data', data => {
+  console.log(data);
+});
+
+// Monitor for command events and send to server
+rl.on('line', command => {
+  conn.on('close',process.exit);
+  conn.write(command);
+});
--- server.js
+++ server.js
@@ -1,13 +1,43 @@
 const net = require('net');
+const fs = require('fs');
 const server = net.createServer();
 const { PORT, HOST } = require('./constants');
 
 server.listen(PORT,() => {
-  console.log(`Server listening on port ${PORT}`);
+  console.log(`Server running at http://${HOST}:${PORT}/`);
 });
 
 server.on('connection', client => {
-  console.log("A client has connected");
-  client.write("Welcome to this file serve")
+  console.log("Client connected");
+  client.setEncoding('utf-8');
+  client.write("Welcome to this file server. To view commands type 'help'.");
+
+  client.on('data', command => {
+    console.log(command);
+    
+    if (command === 'help') {
+      client.write("All Commands:\n  download <filename> (self explanatory)\n  list (lists all files available)\n  help (see this again)");
+    }
+
+    else if (command.toLowerCase().startsWith("list")) {
+      let allFiles = "All Available Files:";
+      fs.readdir('./server_files', (err, files) => {
+        files.forEach((file,index) => allFiles += `\n ${index}  ${file}`);
+        client.write(allFiles);
+      });
+    }
+
+    else if (command.toLowerCase().startsWith("download ")) {
+      const fname = command.replace(/download /, '');
+      client.write(fname);
+    }
+
+    else {
+      client.write(`${command} is not a valid command`);
+    }
+
+  });
+
+  client.on('close', () => console.log("Client disconnected"));
 
 });
